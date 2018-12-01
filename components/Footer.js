b82eb185ba127f9f12a7c705fd0899e2be45fb40
Joe Previte <jjprevite@gmail.com>
2018-12-01 11:56:47 -0700

    Create intro slide

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
+import React from 'react'
 import styled from 'styled-components'
 
-export default styled.footer`
+const Container = styled.footer`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -12,3 +13,9 @@ export default styled.footer`
     margin: 0;
   }
 `
+export default () => (
+  <Container>
+    <a href='https://www.meetup.com/Phoenix-ReactJS/events/256303585/' target='_blank' alt='Phoenix ReactJS lightening talks in December event page'><p style={{ margin: '10px 0' }}>Lightening Talks - Phoenix ReactJS</p></a>
+    <p style={{ margin: '0' }}>Wednesday, December 5th, 2018</p>
+  </Container>
+)

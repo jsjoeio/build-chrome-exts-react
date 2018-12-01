47b9a57578caf4996b00630d03c7a1b430420e2d
Joe Previte <jjprevite@gmail.com>
2018-12-01 11:55:21 -0700

    Create Footer component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0;
+  font-size: 16px;
+
+  * > {
+    margin: 0;
+  }
+`

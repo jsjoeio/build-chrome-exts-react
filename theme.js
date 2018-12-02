333709f64ff53ca4c319b4202d6f22a165d08afc
Joe Previte <jjprevite@gmail.com>
2018-12-02 16:02:05 -0700

    Add colors to theme

--- theme.js
+++ theme.js
@@ -2,9 +2,11 @@ import theme from 'mdx-deck/themes'
 
 export default {
   ...theme,
-
-  // Customize your presentation theme here.
-  //
+  colors: {
+    ...theme.colors,
+    text: '#111111',
+    background: '#FFDC00',
+  }
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
